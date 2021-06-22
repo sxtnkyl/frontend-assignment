8dba8991eff02f0a06c066d382894e2cbab62af9
Solar Olugebefola <solar@iobeam.com>
2021-06-22 00:44:19 -0400

    fix some layout issues

--- src/components/Navbar/logo.jsx
+++ src/components/Navbar/logo.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import "./logo.css";
 
+//<img> tag without src attribute lead to broken image icon
 const Logo = () => {
   return (
     <div className="logo-container">
-      <img className="logo" onError="this.onerror=null;" alt="Timescale logo" />
+      <div className="logo" onError="this.onerror=null;" alt="Timescale logo" />
     </div>
   );
 };
